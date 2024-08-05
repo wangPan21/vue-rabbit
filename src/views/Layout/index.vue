c3fd98331c53d932d614ef17970bce8c21b664ad
攀朔 <panshuo@panshuodeMacBook-Pro.local>
2024-08-05 23:10:04 +0800

    Layout静态结构搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,16 +1,14 @@
 <template>
-    <div>
-        我是首页
-        <!-- 二级路由的出口 -->
-        <router-view></router-view>
-    </div>
+    <LayoutNav></LayoutNav>
+    <LayoutHeader></LayoutHeader>
+    <router-view></router-view>
+    <LayoutFooter></LayoutFooter>
 </template>
 
 <script setup>
-
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
 </script>
 
-<style lang="scss" scoped>
-
-
-</style>
+<style lang="scss" scoped></style>
