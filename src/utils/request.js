91c5540842cd66deb621a09b8d4a3d5e22c550c0
攀朔 <panshuo@panshuodeMacBook-Pro.local>
2024-08-10 18:06:54 +0800

    token 401拦截处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import 'element-plus/es/components/message/style/css'
 import { ElMessage } from 'element-plus'
 import { useUserStore } from "@/stores/user";
+import router from '@/router'
 
 const request = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -21,11 +22,19 @@ request.interceptors.request.use(config => {
 
 //响应拦截器
 request.interceptors.response.use(res => res.data, e => {
+    // const router = useRouter()
+    const userStore = useUserStore()
     //统一错误提示
     ElMessage({
-        type:'error',
-        message:e.response.data.msg
+        type: 'error',
+        message: e.response.data.msg
     })
+    //401token失效处理
+    if (e.response.status === 401) {
+        userStore.logOut()
+        router.push({path:'/login'})
+
+    }
     return Promise.reject(e)
 })
 
