aded9c93b6b2e2984c0f14a42ed3918f9d504cc3
攀朔 <panshuo@panshuodeMacBook-Pro.local>
2024-08-13 15:09:38 +0800

    会员中心渲染

--- src/router/index.js
+++ src/router/index.js
@@ -42,6 +42,20 @@ const router = createRouter({
           path: 'paycallback',
           component: () => import('@/views/Pay/PayBack.vue')
         },
+        {
+          path: 'member',
+          component: () => import('@/views/Member/index.vue'),
+          children:[
+            {
+              path:'user',
+              component:()=>import('@/views/Member/components/UserInfo.vue')
+            },
+            {
+              path:'order',
+              component:()=>import('@/views/Member/components/UserOrder.vue')
+            },
+          ]
+        },
       ]
     },
     {
