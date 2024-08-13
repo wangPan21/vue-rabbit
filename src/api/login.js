b415cfe16fe6e24f8025548faca1af3bc5538f86
攀朔 <panshuo@panshuodeMacBook-Pro.local>
2024-08-13 15:26:02 +0800

    会员中心-个人中心渲染

--- src/api/login.js
+++ src/api/login.js
@@ -1,5 +1,9 @@
 import request from '@/utils/request'
 
 //用户登录
-export const reqGetLoginApi = (account,password) =>
-    request({url:'/login',method:'post',data:{account,password}})
+export const reqGetLoginApi = (account, password) =>
+    request({ url: '/login', method: 'post', data: { account, password } })
+
+//获取猜你喜欢接口
+export const getLikeListAPI = ({ limit = 4 }) =>
+    request({ url: '/goods/relevant', params: { limit } })
